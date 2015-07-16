2db1c5da6a4037a41553222eea6e8f47ad4f94a0
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-07-16 00:26:46 -0400

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    minify = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default', fuction() {
+    console.log('Staring Default Build');
+});
