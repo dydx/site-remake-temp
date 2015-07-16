a0ab2230db96f7adefb3454cae8d16e7a4a2e760
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-07-16 04:28:16 -0400

    got a good build pipeline for this template

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,36 +1,22 @@
 var gulp        = require('gulp'),
-    browserify  = require('browserify'),
     sass        = require('gulp-ruby-sass'),
-    webp        = require('gulp-webp'),
-    uglify      = require('gulp-uglify'),
-    source      = require('vinyl-source-stream'),
-    buffer      = require('vinyl-buffer');
+    uncss       = require('gulp-uncss'),
+    minify      = require('gulp-minify-css'),
+    rename      = require('gulp-rename');
 
 gulp.task('sass-minification', function() {
-    // API for gulp-ruby-sass is different than gulp-sass :(
-    return sass('assets/sass', {style: 'compressed' })
+    return sass('assets/sass', { style: 'expanded' })
+        .pipe(uncss({
+            html: ['index.html']
+        }))
+        .pipe(rename('main.un.css'))
         .pipe(gulp.dest('assets/css'));
-});
-
-gulp.task('scripts-minification', function() {
-    var b = browserify({
-        entries: 'assets/js/main.js',
-        debug: true
-    });
-
-    b.bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(gulp.dest('assets/js'));
 });
 
 gulp.task('watch', function() {
     gulp.watch('assets/sass/*.scss', ['sass-minification']);
-    gulp.watch('assets/js/*.js', ['scripts-minification']);
-    gulp.watch('assets/images', ['images-optimization']);
 });
 
 gulp.task('default', [
     'sass-minification',
-    'scripts-minification'
 ]);
