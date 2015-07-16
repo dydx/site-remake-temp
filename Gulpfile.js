e7bc3646f12dc627e6ad5739201cd2c2a1befdfd
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-07-16 16:29:17 -0400

    lots of optimization shite in Gulp with UnCSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,40 @@ var gulp        = require('gulp'),
     sass        = require('gulp-ruby-sass'),
     uncss       = require('gulp-uncss'),
     minify      = require('gulp-minify-css'),
-    rename      = require('gulp-rename');
+    rename      = require('gulp-rename'),
+    webp        = require('gulp-webp');
 
+// pipe sass through precompiler and then
+// reference against index.html to remove
+// unused styles. HUGE SAVINGS!
 gulp.task('sass-minification', function() {
     return sass('assets/sass', { style: 'expanded' })
+        .pipe(rename('main.css'))
         .pipe(uncss({
             html: ['index.html']
         }))
-        .pipe(rename('main.un.css'))
+        .pipe(minify({ processImport: false }))
         .pipe(gulp.dest('assets/css'));
 });
+
+// reference font-awesome against index.html
+// to remove unused styles. HUGE SAVINGS!
+gulp.task('font-awesome-optimization', function() {
+    return gulp.src('assets/css/font-awesome.min.css')
+        .pipe(rename('fa-un.css'))
+        .pipe(uncss({
+            html: ['index.html']
+        }))
+        .pipe(minify({ processImport: false }))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('image-optimization', function() {
+    return gulp.src('images/header.jpg')
+    .pipe(webp())
+    .pipe(gulp.dest('images'));
+});
+    
 
 gulp.task('watch', function() {
     gulp.watch('assets/sass/*.scss', ['sass-minification']);
@@ -19,4 +43,6 @@ gulp.task('watch', function() {
 
 gulp.task('default', [
     'sass-minification',
+    'font-awesome-optimization',
+    'image-optimization'
 ]);
